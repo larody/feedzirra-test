7f96c91a7788d68754c2b6c3cc6f70ddb73d0e29
larody <throwbases@gmail.com>
2013-05-04 20:01:29 +0900

    new project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FeedzirraTest::Application.config.secret_token = '5412acacac0dae02a31f6e79cb94870b6e562244ab5397f508aa75922e61cd707777920afa7badc6bfac94a8c813372836b2c79f6145425cb00d37f01a6f2f72'
